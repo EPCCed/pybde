c3cd985268d85302fedc7720f4b57a35f32116dc
Ally Hume <a.hume@ed.ac.uk>
2019-06-28 16:37:27 +0100

    Increased to version 4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
         
 setup(name='pybde',
-      version='0.3',
+      version='0.4',
       description='Boolean Delay Equation simulator',
       long_description=readme(),
       long_description_content_type='text/markdown',
